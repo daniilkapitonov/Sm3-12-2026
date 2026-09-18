4ffcf0c53a4051031b0b8de4a10d9581a1a74130
Kapitonov Daniil <62303616+daniilkapitonov@users.noreply.github.com>
2026-09-18 09:51:53 +0300

    Week 3

---
+++ Week 3/bool.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a=7;
+    printf("%d\n",!(a>4));//унирный минус
+    printf("%d\n",a>4 && a>7);//and
+    printf("%d\n",a>4 || a>9);//or
+    return 0;    
+}
+
+
+
+
+
+
+
+
+
+
